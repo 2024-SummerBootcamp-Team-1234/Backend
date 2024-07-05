2744ec91a3c69f12f0c20b539de1c0652d7a402b
sanghun <vitash1215@gmail.com>
2024-07-06 03:10:06 +0900

    feat : swagger tag묶기 및 추가

--- testapp/views.py
+++ testapp/views.py
@@ -5,10 +5,19 @@ import json
 from .models import *
 from rest_framework.response import Response
 from .serializer import *
-
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class SignupView(APIView):
+    @swagger_auto_schema(
+        request_body=UserCreateSerializer,
+        responses={
+            201: UserSerializer,
+            400: 'Bad Request'
+        },
+        tags = ['회원 관련'],
+        operation_description="Create a new user account."
+    )
     def post(self, request):
         serializer = UserCreateSerializer(data=request.data)
 
@@ -22,11 +31,17 @@ class SignupView(APIView):
 
 
 class CategoriesView(APIView):
+    @swagger_auto_schema(
+        tags=['카테고리 관련'],
+    )
     def get(self, request):
         categories = Category.objects.all().values()
         return Response(list(categories), safe=False, status=status.HTTP_200_OK)
 
 class ChannelCreateView(APIView):
+    @swagger_auto_schema(
+        tags=['채널 관련'],
+    )
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -40,6 +55,9 @@ class ChannelCreateView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class SendMessageView(APIView):
+    @swagger_auto_schema(
+        tags=['채널 관련'],
+    )
     def post(self, request, channel_id):
         try:
             data = json.loads(request.body)
@@ -50,6 +68,9 @@ class SendMessageView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class ChannelResultsView(APIView):
+    @swagger_auto_schema(
+        tags=['채널 관련'],
+    )
     def get(self, request):
         channel_id = request.GET.get('channel_id')
         # 여기에 채널 결과를 반환하는 로직을 추가하세요
@@ -58,6 +79,9 @@ class ChannelResultsView(APIView):
 #----------------------------------------------------------------------------------------------------------------------------#
 
 class PostCreateView(APIView):
+    @swagger_auto_schema(
+        tags=['게시판 관련'],
+    )
     def post(self, request):
         try:
             data = json.loads(request.body)
@@ -77,6 +101,13 @@ class PostCreateView(APIView):
 # 조회 방법 - 모든 게시판 조회
 
 class AllPostGetView(APIView):
+    @swagger_auto_schema(
+        responses={
+            200: PostSerializer,
+            400: 'Bad Request'
+        },
+        tags=['게시판 관련'],
+    )
     def get(self, request):
         posts = Post.undeleted_objects.all()
 
@@ -88,6 +119,13 @@ class AllPostGetView(APIView):
 
 # 특정 유저의 게시판 조회
 class UserPostsGetView(APIView):
+    @swagger_auto_schema(
+        responses={
+            200: PostSerializer,
+            405: 'Not found'
+        },
+        tags=['게시판 관련'],
+    )
     def get(self, request, user_id):
         try:
             # get_queryset 메서드를 호출하여 필터링된 쿼리셋을 가져옵니다.
@@ -104,6 +142,9 @@ class UserPostsGetView(APIView):
 
 
 class PostUpdateView(APIView):
+    @swagger_auto_schema(
+        tags=['게시판 관련'],
+    )
     def put(self, request, post_id):
         try:
             data = json.loads(request.body)
@@ -118,6 +159,9 @@ class PostUpdateView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class PostVoteView(APIView):
+    @swagger_auto_schema(
+        tags=['게시판 관련'],
+    )
     def put(self, request, post_id):
         try:
             data = json.loads(request.body)
@@ -135,6 +179,9 @@ class PostVoteView(APIView):
 #----------------------------------------------------------------------------------------------------------------------------#
 # 소프트 삭제 구현 방법
 class PostDeleteView(APIView):
+    @swagger_auto_schema(
+        tags=['게시판 관련'],
+    )
     def delete(self, request, post_id):
         try:
             # 게시물 조회
