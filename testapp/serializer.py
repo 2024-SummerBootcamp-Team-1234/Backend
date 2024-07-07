c13f693a838cdaa1064752391dbcccef421051d6
sanghun <vitash1215@gmail.com>
2024-07-07 18:20:34 +0900

    Help/#8 serializer, swagger, softdeletemanager 적용 (#9)
    * 게시판CRUD제작중
    
    * fix : serializer 수정과 소프트 삭제 구현
    
    * fix : 추상화 객체 설정
    현# test : 테스트 코드 추가
    
    * docs : gitignore 수정
    
    * fix : user 게시판 조회 수정
    
    * feat : swagger tag묶기 및 추가
    
    ---------
    
    Co-authored-by: 이수민 <sammy1313@naver.com>

---
+++ testapp/serializer.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from testapp.models import *
+
+
+#--------------------------------------------------------------#
+# User 관련
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['name', 'email','password']
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email','password']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','name', 'email']
+
+#--------------------------------------------------------------#
+# 게시판 관련
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['host','title','content']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['host','title','content','vote','created_at']
+
+
+
+#--------------------------------------------------------------#
--- testapp/urls.py
+++ testapp/urls.py
@@ -1,7 +1,24 @@
-from django_backend.urls import path
-
-from . import views
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', views.index),
+    path('users/signup', SignupView.as_view(), name='user_signup'),
+
+
+    path('categories', CategoriesView.as_view(), name='categories_list'),
+
+
+    path('channels', ChannelCreateView.as_view(), name='channel_create'),
+    path('channels/messages/<int:channel_id>', SendMessageView.as_view(), name='send_message'),
+    path('channels/results', ChannelResultsView.as_view(), name='channel_results'),
+
+
+    path('posts', PostCreateView.as_view(), name='post_create'),
+    path('posts/all', AllPostGetView.as_view(), name='get_all_post'),  # Adjusted URL for retrieving all posts
+    path('posts/user/<int:user_id>', UserPostsGetView.as_view(), name='get_user_post'),
+    # Adjusted URL for user-specific posts
+    path('posts/<int:post_id>/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/votes/<int:post_id>', PostVoteView.as_view(), name='post_vote'),
+    path('posts/<int:post_id>', PostDeleteView.as_view(), name='post_delete'),
+    # Adjusted URL for deleting a post
 ]
