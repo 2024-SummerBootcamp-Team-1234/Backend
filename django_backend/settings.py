3163a0f0031fd5c9935cdd0f89a27748a9cebbce
myun1022 <80511975+myun1022@users.noreply.github.com>
2024-07-12 17:49:46 +0900

    feat : token 관련 middleware 설정

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user.middleware.RefreshTokenMiddleware',
 ]
 
 ROOT_URLCONF = 'django_backend.urls'
