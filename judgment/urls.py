e792a6f13024e226ebb754633b8b1728e3a2a195
sanghun <vitash1215@gmail.com>
2024-07-15 22:54:57 +0900

    feat : 일단 스탑

--- judgment/urls.py
+++ judgment/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('judgments/',ChatView.as_view(), name=''),
-    path('judgments/', SummaryView.as_view(), name=''),
-]
+# from django.urls import path
+# from .views import *
+#
+# urlpatterns = [
+#     path('judgments/',ChatView.as_view(), name=''),
+#     path('judgments/', SummaryView.as_view(), name=''),
+# ]
