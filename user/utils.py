5166aae1104c6838b05740b60ae969c69661ce31
myun1022 <80511975+myun1022@users.noreply.github.com>
2024-07-18 17:17:55 +0900

    refactor : user/utils.py 응답 형식 변경

--- user/utils.py
+++ user/utils.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from user.models import User
 from user.serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
 
 #스웨거 테스트를 위한 함수
 # 요청에서 토큰 추출 함수
@@ -24,20 +27,20 @@ def get_user_id_from_token(request):
         # access = request.COOKIES.get('access') # 원래 코드
         access = get_token_from_request(request) # 스웨거 테스트를 위한 코드
         if not access:
-            return None, {"message": "No access token provided", "status": 401}
+            return None, Response({"message": "No access token provided"}, status=status.HTTP_401_UNAUTHORIZED)
 
         payload = jwt.decode(access, settings.JWT_SECRET_KEY, algorithms=['HS256'])
         user_id = payload.get('user_id')
         if not user_id:
-            return None, {"message": "Invalid token", "status": 401}
+            return None,  Response({"message": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
 
         return user_id, None
 
     except jwt.ExpiredSignatureError:
-        return None, {"message": "Token has expired", "status": 401}
+        return None, Response({"message": "Token has expired"}, status=status.HTTP_401_UNAUTHORIZED)
 
     except jwt.InvalidTokenError:
-        return None, {"message": "Invalid token", "status": 401}
+        return None, Response({"message": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
 
 # 사용자 객체 반환
 def get_user_from_token(request):
@@ -45,19 +48,19 @@ def get_user_from_token(request):
         # access = request.COOKIES.get('access') # 원래 코드
         access = get_token_from_request(request) # 스웨거 테스트를 위한 코드
         if not access:
-            return {"message": "No access token provided", "status": 401}
+            return Response({"message": "No access token provided"}, status=status.HTTP_401_UNAUTHORIZED)
 
         payload = jwt.decode(access, settings.JWT_SECRET_KEY, algorithms=['HS256'])
         user_id = payload.get('user_id')
         if not user_id:
-            return {"message": "Invalid token", "status": 401}
+            return Response({"message": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
 
         user = get_object_or_404(User, pk=user_id)
         serializer = RegisterSerializer(user)
         return serializer.data
 
     except jwt.ExpiredSignatureError:
-        return {"message": "Token has expired", "status": 401}
+        return Response({"message": "Token has expired"}, status=status.HTTP_401_UNAUTHORIZED)
 
     except jwt.InvalidTokenError:
-        return {"message": "Invalid token", "status": 401}
+        return Response({"message": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
